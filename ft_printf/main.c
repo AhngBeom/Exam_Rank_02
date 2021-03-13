7b08019efe05e61ae42b56300bf7279cf90e31db
AhngBeom <bbu0704@gmail.com>
2021-03-13 16:38:27 +0900

    ft_printf_1try

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -5,7 +5,9 @@ int ft_printf(const char *, ... );
 
 int main()
 {
+	printf(" (return : %d)\n", ft_printf("Hello World !"));
 
+	printf("=============String Format===============\n");
 	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.s|", "abcde"));
 	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.0s|", "abcde"));
 	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.3s|", "abcde"));
@@ -14,14 +16,18 @@ int main()
 	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.7s|", NULL));
 	
 	
+	printf("=============Integer Format===============\n");
 	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.d|", 12345));
 	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.0d|", 12345));
 	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.3d|", 12345));
 	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.7d|", 12345));
-	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.7d|", INT_MIN));
-	//printf(" (return : %d)\n", ft_printf("ft_printf : |%10.7d|", INT_MIN - 1));
+	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.5d|", INT_MIN));
+	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.15d|", INT_MIN - 1));
+	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.5d|", INT_MAX));
+	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.14d|", INT_MAX + 1));
 
 	
+	printf("=============Hex Format===============\n");
 	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.x|", 65535));
 	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.0x|", 65535));
 	printf(" (return : %d)\n", ft_printf("ft_printf : |%10.3x|", 65535));
