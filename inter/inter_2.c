f70e8553bc23d8d583ba8e26d48cfe437379943c
AhngBeom <bbu0704@gmail.com>
2021-03-14 15:29:13 +0900

    gnl_3try

--- inter/inter_2.c
+++ inter/inter_2.c
@@ -1,18 +1,25 @@
 #include <unistd.h>
 
-int	dupl_check(char *str, char ch, int start)
+int	dupl_check(char	*str, char ch, int index)
 {
-	while (start >= 0)
+	int	i;
+
+	if (!str)
+		return (-1);
+	i = 0;
+	while (str[i] != '\0' && i < index)
 	{
-		if (str[start--] == ch)
-			return (1);
+		if (str[i] == ch)
+			return (i);
+		i++;
 	}
-	return (0);
+	return (-1);
 }
 
 int	main(int argc, char *argv[])
 {
-	int i, j;
+	int 	i;
+	int	j;
 
 	if (argc == 3)
 	{
@@ -24,10 +31,10 @@ int	main(int argc, char *argv[])
 			{
 				if (argv[1][i] == argv[2][j])
 				{
-					if (!dupl_check(argv[1], argv[1][i], i - 1))  
-					{
+					if (!(dupl_check(argv[1], argv[1][i], i) >= 0))
+					{	
 						write(1, &argv[1][i], 1);
-						break;
+						break ;
 					}
 				}
 				j++;
--- union/union_1.c
+++ union/union_1.c
@@ -13,28 +13,28 @@ int	dupl_check(char *str, char ch, int start)
 int	main(int argc, char *argv[])
 {
 	int i, j;
-
 	if (argc == 3)
 	{
 		i = 0;
 		while (argv[1][i] != '\0')
 		{
-			j = 0;
-			while (argv[2][j] != '\0')
+			if (!dupl_check(argv[1], argv[1][i], i - 1))
 			{
-				if (argv[1][i] == argv[2][j])
-				{
-					if (!dupl_check(argv[1], argv[1][i], i - 1))  
-					{
-						write(1, &argv[1][i], 1);
-						break;
-					}
-				}
-				j++;
+				write(1, &argv[1][i], 1);
 			}
 			i++;
 		}
+		j = 0;
+		while (argv[2][j] != '\0')
+		{
+			if (!dupl_check(argv[1], argv[2][j], i - 1) && 
+					!dupl_check(argv[2], argv[2][j], j - 1))
+			{
+				write(1, &argv[2][j], 1);
+			}
+			j++;
+		}
 	}
-	write(1, "\n", 1);
+	write (1, "\n", 1);
 	return (0);
 }
